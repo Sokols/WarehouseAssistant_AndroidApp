88048e3f28250908a7a1d6e64dbb0ac4e1a3c7fe
Igor Sokół <isokol@freeportmetrics.com>
2023-01-31 16:43:44 +0100

    Update Firebase connection

--- app/src/main/java/pl/sokols/warehouseassistant/utils/FirebaseDatabaseUtils.kt
+++ app/src/main/java/pl/sokols/warehouseassistant/utils/FirebaseDatabaseUtils.kt
@@ -8,7 +8,7 @@ import com.google.firebase.database.FirebaseDatabase
 object FirebaseDatabaseUtils {
 
     private const val FIREBASE_URL =
-        "https://warehouseassistant-default-rtdb.europe-west1.firebasedatabase.app"
+        "https://warehouse-assistant-88a9e-default-rtdb.europe-west1.firebasedatabase.app"
 
     fun getUserTableReference(): DatabaseReference = FirebaseDatabase
         .getInstance(FIREBASE_URL)
